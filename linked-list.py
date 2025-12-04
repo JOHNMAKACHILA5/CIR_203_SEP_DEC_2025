a9de0a4950dd449c9a5b72946f711fe0cede0c90
JOHN  AMISI ACHILA <jamisi410@gmail.com>
2025-12-05 00:17:38 +0300

    Add files via upload

---
+++ linked-list.py
@@ -0,0 +1,11 @@
+Student_inf={"Name":"John",
+             next:{
+                 "Adm_No.":"CIT/00074/024",
+             next:{
+                 "units":["CIR 203","CIS211","CIT 201"],
+                 next:{
+                    "grade":["A","B","B"],
+                     next:{}}}}}
+print (Student_inf)
+
+
